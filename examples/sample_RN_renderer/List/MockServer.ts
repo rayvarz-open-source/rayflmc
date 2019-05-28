fc6470b8d3d413a91df5a803488f9332ebfab18a
Sahand <sahandevs@gmail.com>
2019-05-28 13:59:01 +0430

    added a mock server

---
+++ examples/sample_RN_renderer/List/MockServer.ts
@@ -0,0 +1,45 @@
+import { CodableModel } from "../flmc/Base/IModel";
+
+export interface ResultCallBACK {
+    (result: SampleModel[]): void
+}
+
+export class SampleModel extends CodableModel {
+
+    constructor(public name: string = "", public description: string = "") { super() }
+
+}
+
+const NUMBER_OF_ITEMS_IN_A_PAGE = 4;
+
+export class MockDataSource {
+
+    fakeData: SampleModel[] = [
+        new SampleModel("Sahand", "This is a sample long description"),
+        new SampleModel("Ali", "This is a sample long desc"),
+        new SampleModel("Mamad", "This is a sample long desc"),
+        new SampleModel("Taqi", "This is a !!! long description"),
+        new SampleModel("Akbar", "This is a sample long description☺"),
+        new SampleModel("Morteza", "This is a sample long description"),
+        new SampleModel("Sahand", "This is a sample long description"),
+        new SampleModel("Sahand", "This ☺is a sample long description"),
+        new SampleModel("Sahand", "☺This is a sample long description"),
+        new SampleModel("Sahand", "This is a sample long description"),
+        new SampleModel("Sahand", "This is a sample long description"),
+        new SampleModel("Sahand", "This is a☺ sample long description"),
+        new SampleModel("Sahand", "This is a☺ sample long description"),
+        new SampleModel("Sahand", "This is a☺ sample long description"),
+        new SampleModel("☺", "This is a☺ sample long description"),
+        new SampleModel("☺", "This is a☺ sample long description"),
+        new SampleModel(":)", "This is a☺ sample long description"),
+    ];
+
+    getData(pageNo: number = 0, search: "", callback: ResultCallBACK): void {
+        setTimeout(() => {
+            callback(this.fakeData
+                .filter(item => item.name.search(search) != -1 || item.description.search(search) != -1)
+                .slice(pageNo * NUMBER_OF_ITEMS_IN_A_PAGE, (pageNo + 1) * NUMBER_OF_ITEMS_IN_A_PAGE));
+        }, 3000);
+    }
+
+}
