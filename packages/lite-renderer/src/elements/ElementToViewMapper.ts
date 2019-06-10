dfaf7978545486cc62297b88c8d9de41b02b6430
Sahand <sahandevs@gmail.com>
2019-06-10 13:47:35 +0430

    Create ElementToViewMapper.ts

---
+++ packages/lite-renderer/src/elements/ElementToViewMapper.ts
@@ -0,0 +1,22 @@
+import IElement from "flmc-data-layer/src/FormController/IElement";
+import { ElementType } from "./ElementType";
+import * as React from 'react'
+
+import ButtonView from './button/ButtonView';
+import { ButtonElement } from "./button/ButtonElement";
+import LabelView from "./label/LabelView";
+import { LabelElement } from "./label/LabelElement";
+import ContainerView from "./container/ContainerView";
+import { ContainerElement } from "./container/ContainerElement";
+import TextInputView from "./input/text/TextInputView";
+import { TextInputElement } from "./input/text/TextInputElement";
+
+export function mapToView(element: IElement): React.ReactElement {
+    switch (element.type) {
+        case (ElementType.BUTTON): return ButtonView({element: element as ButtonElement});
+        case (ElementType.LABEL): return LabelView({element: element as LabelElement});
+        case (ElementType.CONTAINER): return ContainerView({element: element as ContainerElement});
+        case (ElementType.INPUT_TEXT): return TextInputView({element: element as TextInputElement});
+    }
+    throw Error(`can't map ${element.type} to a view`)
+}
