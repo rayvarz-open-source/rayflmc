aeb219eee8e24967a4e22efad4b7213f5d52b509
Sahand <sahandevs@gmail.com>
2019-06-10 13:52:56 +0430

    format and create CreateForm

---
+++ packages/lite-renderer/src/form/CreateForm.ts
@@ -0,0 +1,9 @@
+import * as React;
+import FormController from "flmc-data-layer/src/FormController/FormController";
+import Container from "./elements/container/ContainerElement";
+import ContainerView from "./elements/container/ContainerView";
+
+export default function createForm(controller: FormController): React.ReactElement {
+    let container = Container(controller.elements);
+    return ContainerView({ element: container });
+}
