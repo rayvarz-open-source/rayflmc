6c02a46403adec128cd96a2dfdefb605700dee56
Sahand <sahandevs@gmail.com>
2019-06-10 12:31:55 +0430

    implement ButtonView

---
+++ packages/lite-renderer/src/elements/button/ButtonView.tsx
@@ -0,0 +1,29 @@
+import { ButtonElement } from './ButtonElement';
+import Button from '@material-ui/core/Button';
+import * as React from 'react';
+
+type Props = {
+    element: ButtonElement
+}
+
+export default function ButtonView({ element }: Props) {
+
+    const [onClick, setOnClick] = React.useState(() => { });
+    const [title, setTitle] = React.useState(() => { });
+
+    element.buttonCallback.subscribe({
+        next: (v) => setOnClick(v == null ? () => { } : v)
+    });
+
+    element.buttonText.subscribe({
+        next: (v) => setTitle(v)
+    });
+
+
+    return (
+        <Button variant="contained" onClick={onClick}>
+            {title}
+        </Button>
+    )
+
+}
