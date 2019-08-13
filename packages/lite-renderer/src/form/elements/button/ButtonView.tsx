50e3023b8a1e5c214e61d34528eca37cda8ef2e8
Sahandevs <sahandevs@gmail.com>
2019-08-13 17:58:39 +0430

    fix progress indicator color in Button

--- packages/lite-renderer/src/form/elements/button/ButtonView.tsx
+++ packages/lite-renderer/src/form/elements/button/ButtonView.tsx
@@ -1,18 +1,19 @@
-import { Button, CircularProgress, createMuiTheme, Icon as MIcon, withStyles } from '@material-ui/core';
-import * as React from 'react';
-import useFunctionAsState from '../../../custom-hooks/function-state';
-import { Visibility } from '../base/BaseElement';
-import { ButtonElement } from './ButtonElement';
-import { Colors, Disabled, Icon, Loading, OnClick, Text, Variant } from './ButtonElementAttributes';
+import { Button, CircularProgress, createMuiTheme, Icon as MIcon, withStyles } from "@material-ui/core";
+import * as React from "react";
+import useFunctionAsState from "../../../custom-hooks/function-state";
+import { Visibility } from "../base/BaseElement";
+import { ButtonElement } from "./ButtonElement";
+import { Colors, Disabled, Icon, Loading, OnClick, Text, Variant } from "./ButtonElementAttributes";
+import { useTheme } from "@material-ui/styles";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
 
 type Props = {
-  element: ButtonElement,
-  weight: number
-}
+  element: ButtonElement;
+  weight: number;
+};
 
 export default function ButtonView({ element, weight }: Props) {
-
-  const theme = createMuiTheme()
+  const theme = useTheme<Theme>();
 
   //region generated
   /*******************************************/
@@ -21,14 +22,13 @@ export default function ButtonView({ element, weight }: Props) {
   const [text, setText] = React.useState<Text>(undefined);
   const [loading, setLoading] = React.useState<Loading>(false);
   const [disabled, setDisabled] = React.useState<Disabled>(false);
-  const [colors, setColors] = React.useState<Colors>('default');
-  const [variant, setVariant] = React.useState<Variant>('contained');
+  const [colors, setColors] = React.useState<Colors>("default");
+  const [variant, setVariant] = React.useState<Variant>("contained");
   const [icon, setIcon] = React.useState<Icon>(undefined);
   const [onClick, setOnClick] = useFunctionAsState<OnClick>(undefined);
-  const [visibility, setVisibility] = React.useState<Visibility>('show');
+  const [visibility, setVisibility] = React.useState<Visibility>("show");
 
   React.useEffect(() => {
-
     let textSub = element.textContainer.subscribe({ next: v => setText(v) });
     let loadingSub = element.loadingContainer.subscribe({ next: v => setLoading(v) });
     let disabledSub = element.disabledContainer.subscribe({ next: v => setDisabled(v) });
@@ -55,38 +55,38 @@ export default function ButtonView({ element, weight }: Props) {
   //endregion
 
   function getProgressColor(variant: Variant, color: Colors) {
-      return "#000000"  // TODO: USE THEME
+    if (theme === null) return "#000000";
+    if (variant === "contained") return theme.palette.primary.light;
+    else return theme.palette.primary.dark;
   }
 
   const ColorCircularProgress = withStyles({
     root: {
-      color: getProgressColor(variant, colors),
-    },
+      color: getProgressColor(variant, colors)
+    }
   })(CircularProgress);
 
   function createIcon() {
     if (!icon) return null;
-    return (<MIcon style={{ marginRight: !!text?8:0 }}>{icon}</MIcon>);
+    return <MIcon style={{ marginRight: !!text ? 8 : 0 }}>{icon}</MIcon>;
   }
 
   function createLoading() {
     if (!loading) return null;
-    return (<ColorCircularProgress style={{ marginRight: !!text?8:0 }} size={20} thickness={3} />);
+    return <ColorCircularProgress style={{ marginRight: !!text ? 8 : 0 }} size={20} thickness={3} />;
   }
 
   const handleClick = () => {
     if (loading || onClick == null) return;
     onClick();
-  }
-
+  };
 
   return (
-    <Button style={
-      {
+    <Button
+      style={{
         ...element.getVisibilityStyle(visibility),
         ...element.getWeightStyle(weight)
-      }
-    }
+      }}
       variant={variant as any}
       color={colors}
       disabled={disabled}
@@ -96,7 +96,5 @@ export default function ButtonView({ element, weight }: Props) {
       {createLoading()}
       {text}
     </Button>
-
   );
-
 }
