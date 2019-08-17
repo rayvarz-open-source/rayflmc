968392ee64fb845e14a2baa8a460eea30cd75575
Sahandevs <sahandevs@gmail.com>
2019-08-17 16:55:22 +0430

    Bump Version

--- packages/lite-renderer/src/services/ModalService/ModalService.ts
+++ packages/lite-renderer/src/services/ModalService/ModalService.ts
@@ -1,22 +1,42 @@
-import { ModalElement } from "../../form/elements/modal/ModalElement";
-import IElement from "../../flmc-data-layer/FormController/IElement";
 import { BehaviorSubject } from "rxjs";
 import { filter, skip } from "rxjs/operators";
-import { element } from "prop-types";
+import IElement from "../../flmc-data-layer/FormController/IElement";
+import { ModalElement } from "../../form/elements/modal/ModalElement";
+import { MaxHeight, MaxWidth, MinHeight, MinWidth } from "../../form/elements/modal/ModalElementAttributes";
 export const MODAL_SERVICE_NAME = "LiteRenderer:Services:ModalService";
 
-type Options = { visibleHeader: boolean };
+type Options = {
+  visibleHeader: boolean;
+  maxWidth: MaxWidth;
+  minWidth: MinWidth;
+  maxHeight: MaxHeight;
+  minHeight: MinHeight;
+  noAutoClose: boolean;
+  noPadding: boolean;
+};
 export class ModalService {
   modalElement: ModalElement;
   child = new BehaviorSubject<IElement | undefined>(undefined);
   isOpen = new BehaviorSubject<boolean>(false);
   visibleHeader = new BehaviorSubject<boolean>(true);
+  maxWidth = new BehaviorSubject<MaxWidth>(undefined);
+  minWidth = new BehaviorSubject<MinWidth>(undefined);
+  maxHeight = new BehaviorSubject<MaxHeight>(undefined);
+  minHeight = new BehaviorSubject<MinHeight>(undefined);
+  noAutoClose = new BehaviorSubject<boolean>(true);
+  noPadding = new BehaviorSubject<boolean>(false);
 
   queue: ([IElement, Options])[] = [];
 
   constructor(modalElement: ModalElement) {
     this.modalElement = modalElement
       .child(this.child)
+      .maxWidth(this.maxWidth)
+      .minWidth(this.minWidth)
+      .maxHeight(this.maxHeight)
+      .minHeight(this.minHeight)
+      .noBackdropClickClose(this.noAutoClose)
+      .noBackdropClickClose(this.noAutoClose)
       .visibleHeader(this.visibleHeader)
       .open(this.isOpen);
 
@@ -37,7 +57,18 @@ export class ModalService {
       });
   }
 
-  enqueue(element: IElement, options: Options = { visibleHeader: true }) {
+  enqueue(
+    element: IElement,
+    options: Options = {
+      visibleHeader: true,
+      maxHeight: undefined,
+      maxWidth: undefined,
+      minHeight: undefined,
+      minWidth: undefined,
+      noAutoClose: true,
+      noPadding: false
+    }
+  ) {
     if (this.isOpen.value === false && this.queue.length === 0) {
       this.open(element, options);
       return;
@@ -53,6 +84,12 @@ export class ModalService {
   private open(element: IElement, options: Options) {
     this.child.next(element);
     this.visibleHeader.next(options.visibleHeader);
+    this.maxHeight.next(options.maxHeight);
+    this.minHeight.next(options.minHeight);
+    this.maxWidth.next(options.maxWidth);
+    this.minHeight.next(options.minHeight);
+    this.noAutoClose.next(options.noAutoClose);
+    this.noPadding.next(options.noPadding);
     this.isOpen.next(true);
   }
 }
