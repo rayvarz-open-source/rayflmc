7a4a7ab8bdac9efe479808cdb3b8da44d5f88c9c
Sahandevs <sahandevs@gmail.com>
2019-08-25 17:26:13 +0430

    add routerService to base controller

--- packages/lite-renderer/src/FLMCFormController.ts
+++ packages/lite-renderer/src/FLMCFormController.ts
@@ -1,19 +1,19 @@
 import FormController from "./flmc-data-layer/FormController/FormController";
 import { InjectorReciever } from "./injector/InjectorReciever";
+import { ModalService, MODAL_SERVICE_NAME } from "./services/ModalService/ModalService";
+import { RouterService, ROUTER_SERVICE } from "./services/RouterService/RouterService";
 import { SnackService, SNACK_SERVICE_NAME } from "./services/SnackService";
-import {
-  ModalService,
-  MODAL_SERVICE_NAME
-} from "./services/ModalService/ModalService";
 
 export class FLMCFormController extends FormController
   implements InjectorReciever {
   snackService: SnackService;
   modalService: ModalService;
+  routerService: RouterService;
 
   inject(injector: import("./injector/Injector").Injector) {
     this.snackService = injector.inject<SnackService>(SNACK_SERVICE_NAME)!;
     this.modalService = injector.inject<ModalService>(MODAL_SERVICE_NAME)!;
+    this.routerService = injector.inject<RouterService>(ROUTER_SERVICE)!;
   }
 
   ready() {}
