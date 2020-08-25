19a394b0ec36a0c312b3ac62b2a0dc3a8480ede0
omid <o.payandeh@gmail.com>
2020-08-25 13:01:26 +0430

    Bump Version

--- packages/lite-renderer/src/form/FormView.tsx
+++ packages/lite-renderer/src/form/FormView.tsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
+import * as React from "react";
 import FormController from "../flmc-data-layer/FormController/FormController";
+import { NativeProps } from "./elements/base/BaseElement";
 import Container from "./elements/container/ContainerElement";
 import ContainerView from "./elements/container/ContainerView";
 
 type Props = {
-    controller: FormController
-}
+  controller: FormController;
+  nativeProps?: NativeProps;
+};
 
-export default function FormView({ controller }: Props) {
-    let container = Container(controller.elements);
-    return <ContainerView element={container} weight={0} />
+export default function FormView({ controller, nativeProps }: Props) {
+  let container = Container(controller.elements).nativeProps(
+    nativeProps ? nativeProps : {}
+  );
+  return <ContainerView element={container} weight={0} />;
 }
